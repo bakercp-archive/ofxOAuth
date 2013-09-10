0741f781ecb24e5ac442025525a5d4c3189a0f4d
Christopher Baker <me@christopherbaker.net>
2013-09-09 21:43:19 -0500

    Update Xcode project to new template.  Removed openssl.

---
+++ example-generic/addons.make
@@ -0,0 +1,2 @@
+ofxOAuth
+ofxXmlSettings
---
+++ example-twitter/src/ofApp.cpp
@@ -0,0 +1,71 @@
+// =============================================================================
+//
+// Copyright (c) 2010-2013 Christopher Baker <http://christopherbaker.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
+
+
+#include "ofApp.h"
+
+
+//------------------------------------------------------------------------------
+void ofApp::setup()
+{
+    ofSetFrameRate(30);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    // A simplified interface when using the ofxExampleTwitterClient class.
+    // Twitter-specific configuration details can be taken care of the 
+    // ofxOAuth base class.
+    client.setup("J15RonAmOoXDY3ototqIYw","fL0bsBZCQwUMUXDhJCGwUNEBzkta0cYWpgTamXoB50");
+    
+    // Once authenticated, press the spacebar to try a test call.
+}
+
+//------------------------------------------------------------------------------
+void ofApp::draw()
+{
+    if(client.isAuthorized())
+    {
+        ofBackground(0,255,0);
+    }
+    else
+    {
+        ofBackground(255,0,0);
+    }
+}
+
+//------------------------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+     if(key == ' ')
+     {
+        if(client.isAuthorized())
+        {
+            std::string s = client.exampleMethod();
+            ofLogNotice("testApp::keyPressed") << s;
+        }
+        else
+        {
+            ofLogWarning("testApp::keyPressed") << "Not authorized yet.";
+        }
+    }
+}
